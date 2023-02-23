306cca4340008a96d7fb76c1befd9d0bb58b356e
BojanMarkovic659 <markovicc.b@gmail.com>
2023-02-23 13:54:49 +0100

    Created singly linked list class

---
+++ src/java/dynamicstructures/SinglyLinkedList.java
@@ -0,0 +1,14 @@
+package dynamicstructures;
+
+public class SinglyLinkedList {
+    private Node head;
+
+    private class Node {
+        private int data;
+        private Node next;
+    }
+
+
+
+}
+
