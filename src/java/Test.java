4dd05b7bb45d5eecc255b6ad33b96850865813d3
BojanMarkovic659 <markovicc.b@gmail.com>
2023-02-24 16:11:13 +0100

    Implement singly linked list (#2)
    * Created singly linked list class
    
    * Remove duplicates in singly unsorted and sorted list.

--- src/java/Test.java
+++ src/java/Test.java
@@ -1,10 +1,12 @@
 import definitions.IStack;
+import dynamicstructures.SinglyLinkedList;
 import staticstructures.StaticStack;
 
 public class Test {
 
     public static void main(String[] args) throws Exception {
-        testStaticStack();
+        //testStaticStack();
+        testSinglyLinkedList();
     }
 
     public static void testStaticStack() throws Exception {
@@ -25,6 +27,21 @@ public class Test {
         System.out.println("Stack size " + stack.size());
         System.out.println("---------------------------------------------");
         System.out.println("Number of elements " + stack.count());
+
+    }
+    public static void testSinglyLinkedList(){
+        SinglyLinkedList singlyLinkedList = new SinglyLinkedList();
+
+        singlyLinkedList.addElement(5);
+        singlyLinkedList.addElement(5);
+        singlyLinkedList.addElement(5);
+//        singlyLinkedList.addElement(5);
+//        singlyLinkedList.addElement(1);
+//        singlyLinkedList.addElement(6);
+        singlyLinkedList.displayList();
+        singlyLinkedList.removeDuplicatesUnsortedList();
+        System.out.println("---------------------------------------------");
+        singlyLinkedList.displayList();
 
     }
 
