101b9611134105ae65a83e9514cdfd626a4460c2
BojanMarkovic659 <markovicc.b@gmail.com>
2023-02-28 14:30:56 +0100

    Implemented binary search

--- src/java/Test.java
+++ src/java/Test.java
@@ -1,18 +1,17 @@
 import definitions.IStack;
 import dynamicstructures.SinglyLinkedList;
 import searchalgorithms.SearchAlgorithms;
-import sorting.SortingAlgorithms;
 import staticstructures.StaticStack;
 
 public class Test {
 
-    public static void main(String[] args)  {
+    public static void main(String[] args) {
         try {
             //testStaticStack();
             //testRemoveDuplicatesSortedList();
             //testRemoveDuplicatesUnsortedList();
             testBinarySearchAlgorithms();
-        }catch (Exception exception){
+        } catch (Exception exception) {
             System.err.println("Error: " + exception.getMessage());
         }
 
@@ -111,8 +110,8 @@ public class Test {
         singlyLinkedList.removeAll();
 
 
-
     }
+
     public static void testRemoveDuplicatesUnsortedList() {
         SinglyLinkedList singlyLinkedList = new SinglyLinkedList();
         System.out.println("Testing removeDuplicatesUnsortedList method");
@@ -158,18 +157,21 @@ public class Test {
         System.out.println("---------------------------------------------");
         singlyLinkedList.removeAll();
     }
+
     public static void testBinarySearchAlgorithms() throws Exception {
         SearchAlgorithms searchAlgorithms = new SearchAlgorithms();
-        int[] array = {1,2,3,4,5,6,7,8,9};
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9};
         System.out.println("Expected true, got: " + searchAlgorithms.isSorted(array));
         try {
             System.out.println("Testing arrayBinarySearch method");
-            System.out.println("Expecting result 7: got: " + searchAlgorithms.arrayBinarySearch(array,8));
+            System.out.println("Expecting result 7: got: " + searchAlgorithms.arrayBinarySearch(array, 8));
 
             array = new int[]{1, 5, 7, 3, 8};
             System.out.println("Expected false, got: " + searchAlgorithms.isSorted(array));
 
-
+            array = new int[]{5, 5, 5, 5, 5, 5, 5};
+            System.out.println("Expected true, got: " + searchAlgorithms.isSorted(array));
+            System.out.println("Expecting result 3: got: " + searchAlgorithms.arrayBinarySearch(array, 5));
         } catch (Exception e) {
             System.out.println("Error : " + e.getMessage());
         }
