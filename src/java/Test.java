5fe550a300ef889b0751b0cf13a81ac2b81bf3f1
BojanMarkovic659 <markovicc.b@gmail.com>
2023-02-28 17:20:37 +0100

    Implemented binary search

--- src/java/Test.java
+++ src/java/Test.java
@@ -10,7 +10,8 @@ public class Test {
             //testStaticStack();
             //testRemoveDuplicatesSortedList();
             //testRemoveDuplicatesUnsortedList();
-            testBinarySearchAlgorithms();
+            //testIsSorted();
+            testArrayBinarySearch();
         } catch (Exception exception) {
             System.err.println("Error: " + exception.getMessage());
         }
@@ -158,20 +159,44 @@ public class Test {
         singlyLinkedList.removeAll();
     }
 
-    public static void testBinarySearchAlgorithms() throws Exception {
+    public static void testArrayBinarySearch() throws Exception {
         SearchAlgorithms searchAlgorithms = new SearchAlgorithms();
+
         int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-        System.out.println("Expected true, got: " + searchAlgorithms.isSorted(array));
+        System.out.println("Testing arrayBinarySearch method");
+        System.out.println("Expecting result 7: got: " + searchAlgorithms.arrayBinarySearch(array, 8));
+        array = new int[]{1, 2, 3, 6, 8, 11, 15, 18, 24};
+        System.out.println("Expecting result 1: got: " + searchAlgorithms.arrayBinarySearch(array, 2));
+        System.out.println("Expecting result -1: got: " + searchAlgorithms.arrayBinarySearch(array, 12));
+        array = new int[]{5, 5, 5, 5, 5, 5, 5};
+        System.out.println("Expecting result 3: got: " + searchAlgorithms.arrayBinarySearch(array, 5));
         try {
-            System.out.println("Testing arrayBinarySearch method");
-            System.out.println("Expecting result 7: got: " + searchAlgorithms.arrayBinarySearch(array, 8));
-
             array = new int[]{1, 5, 7, 3, 8};
-            System.out.println("Expected false, got: " + searchAlgorithms.isSorted(array));
+            System.out.println("Expecting result : exception : got: " + searchAlgorithms.arrayBinarySearch(array, 2));
+            array = null;
+            System.out.println("Expecting result exception : got : " + searchAlgorithms.arrayBinarySearch(array, 4));
+        } catch (Exception e) {
+            System.out.println("Error : " + e.getMessage());
+        }
+    }
 
-            array = new int[]{5, 5, 5, 5, 5, 5, 5};
-            System.out.println("Expected true, got: " + searchAlgorithms.isSorted(array));
-            System.out.println("Expecting result 3: got: " + searchAlgorithms.arrayBinarySearch(array, 5));
+    public static void testIsSorted() throws Exception {
+        SearchAlgorithms searchAlgorithms = new SearchAlgorithms();
+
+        int[] array = {1, 2, 3, 4, 7, 9, 11};
+        System.out.println("Testing isSorted method : ");
+        System.out.println("Expected true, got: " + searchAlgorithms.isSorted(array));
+        System.out.println("----------------------------------------");
+        array = new int[]{5, 3, 2, 8, 10};
+        System.out.println("Expected false, got: " + searchAlgorithms.isSorted(array));
+        System.out.println("----------------------------------------");
+        array = new int[]{};
+        System.out.println("Expected true, got: " + searchAlgorithms.isSorted(array));
+        array = new int[]{1, 2, 3, 4, 4, 4, 5, 6, 7};
+        System.out.println("Expected true, got: " + searchAlgorithms.isSorted(array));
+        array = null;
+        try {
+            System.out.println("Expected exception, got: " + searchAlgorithms.isSorted(array));
         } catch (Exception e) {
             System.out.println("Error : " + e.getMessage());
         }
--- src/java/searchalgorithms/SearchAlgorithms.java
+++ src/java/searchalgorithms/SearchAlgorithms.java
@@ -2,7 +2,10 @@ package searchalgorithms;
 
 public class SearchAlgorithms {
 
-    public boolean isSorted(int[] array) {
+    public boolean isSorted(int[] array) throws Exception {
+        if (array == null) {
+            throw new Exception("Given array is null");
+        }
         for (int i = 0; i < array.length - 1; i++) {
             if (array[i] > array[i + 1]) {
                 return false;
@@ -11,23 +14,24 @@ public class SearchAlgorithms {
 
         return true;
     }
-
     public int arrayBinarySearch(int[] array, int element) throws Exception {
-
+        if(array == null){
+            throw new Exception("Given array is null");
+        }
         if (!isSorted(array)) {
             throw new Exception("Array is not sorted in ascending order");
         }
         int start = 0;
-
-        while (start <= array.length - 1) {
-            int mid = (start + array.length - 1) / 2;
+        int end = array.length - 1;
+        while (start <= end) {
+            int mid = (start + end) / 2;
             if (array[mid] == element) {
                 return mid;
             }
             if (array[mid] < element) {
                 start = mid + 1;
             } else {
-                start = mid - 1;
+                end = mid - 1;
             }
         }
         return -1;
