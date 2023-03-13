c99ecef1508ba862f1622d18d58dddca86b0fa5e
BojanMarkovic659 <markovicc.b@gmail.com>
2023-03-13 10:59:40 +0100

    Insertion sort (#7)
    * Insertion sort

--- src/java/Test.java
+++ src/java/Test.java
@@ -4,17 +4,17 @@ import sorting.SortingAlgorithms;
 import searchalgorithms.SearchAlgorithms;
 import staticstructures.StaticStack;
 
-
 public class Test {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         try {
             //testStaticStack();
             //testRemoveDuplicatesSortedList();
             //testRemoveDuplicatesUnsortedList();
             //testIsSorted();
             //testArrayBinarySearch();
-            testArrayBubbleSortAscending();
+            testArrayInsertionSortAscending();
+            //testArrayBubbleSortAscending();
         } catch (Exception exception) {
             System.err.println("Error: " + exception.getMessage());
         }
@@ -244,6 +244,47 @@ public class Test {
             searchAlgorithms.isSorted(array);
         } catch (Exception e) {
             System.out.println("Expected exception, got: " + e.getMessage());
+        }
+    }
+    public static void testArrayInsertionSortAscending() throws Exception{
+        SortingAlgorithms sortingAlgorithms = new SortingAlgorithms();
+        int[] array = {2,7,5,4,10,8};
+        System.out.println("Array before method call: ");
+        sortingAlgorithms.display(array);
+        int[] sortedArray = sortingAlgorithms.arrayInsertionSortAscending(array);
+        System.out.println("Array after method call: ");
+        sortingAlgorithms.display(sortedArray);
+        System.out.println("---------------------------------------");
+        System.out.println("When all items are the same: ");
+        array = new int[]{2,2,2,2,2};
+        sortingAlgorithms.arrayInsertionSortAscending(array);
+        sortingAlgorithms.display(array);
+        System.out.println("---------------------------------------");
+        System.out.println("When two items are the same: ");
+        array = new int[]{2,10,4,3,11,4};
+        sortingAlgorithms.arrayInsertionSortAscending(array);
+        sortingAlgorithms.display(array);
+        System.out.println("---------------------------------------");
+        System.out.println("When array is empty: ");
+        array = new int[]{};
+        sortingAlgorithms.arrayInsertionSortAscending(array);
+        sortingAlgorithms.display(array);
+        System.out.println("---------------------------------------");
+        System.out.println("When array is sorted ascending order: ");
+        array = new int[]{2,6,7,9,12,16};
+        sortingAlgorithms.arrayInsertionSortAscending(array);
+        sortingAlgorithms.display(array);
+        System.out.println("---------------------------------------");
+        System.out.println("When array is sorted descending order: ");
+        array = new int[]{18,11,10,5,3,1};
+        sortingAlgorithms.arrayInsertionSortAscending(array);
+        sortingAlgorithms.display(array);
+        try {
+            array = null;
+            sortingAlgorithms.arrayInsertionSortAscending(array);
+            sortingAlgorithms.display(array);
+        }catch (Exception e){
+            System.out.println("Expecting result : exception : got : "+e.getMessage());
         }
     }
 }
